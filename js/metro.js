948d2ff1bf081b1d11ebed7f8e1741533014f07c
Scoowy <gahonajuanjo@gmail.com>
2018-05-01 12:14:32 -0500

    Add Class active

--- js/metro.js
+++ js/metro.js
@@ -1,9 +1,12 @@
 $(function () {
     $('.ancla').bind('click', function (event) {
-        var $anchor = $(this);
+        var $ancla = $(this);
         $('html, body').stop().animate({
-            scrollLeft: $($anchor.attr('href')).offset().left
+            scrollLeft: $($ancla.attr('href')).offset().left
         }, 1000);
         event.preventDefault();
+
+        $('.ancla.active').removeClass('active');
+        $($(this)).addClass('active');
     });
 });
