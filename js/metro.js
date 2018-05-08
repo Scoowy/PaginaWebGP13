bfb90fcda12b1cf3b3d994705aa09a3067b56241
Scoowy <gahonajuanjo@gmail.com>
2018-05-08 06:27:54 -0500

    Add Completly Page

--- js/metro.js
+++ js/metro.js
@@ -9,4 +9,13 @@ $(function () {
         $('.ancla.active').removeClass('active');
         $($(this)).addClass('active');
     });
+
+    // $(window).on('resize', function () {
+    //     var $ancla = $('.ancla.active');
+    //     $('html, body').stop().animate({
+    //         scrollLeft: $($ancla.attr('href')).offset().left
+    //     }, 1000);
+    //     event.preventDefault();
+    // });
+
 });
